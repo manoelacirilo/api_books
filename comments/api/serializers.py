169068224e71f598cd234d677a83290cb80ccd8d
Manoela Cirilo <manoelacirilo04@gmail.com>
2022-07-18 20:18:46 -0300

    validate logged user on update and destroy comment

--- comments/api/serializers.py
+++ comments/api/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework.serializers import ModelSerializer
 
 from comments.models import Comment
+from users.api.serializers import UserSerializer
 
 
 class CommentSerializer(ModelSerializer):
-    # user = UserSerializer(read_only=True)
+    user = UserSerializer(read_only=True)
 
     class Meta:
         model = Comment
--- comments/api/viewsets.py
+++ comments/api/viewsets.py
@@ -1,4 +1,6 @@
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from comments.models import Comment
@@ -10,3 +12,29 @@ class CommentViewSet(ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated]
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def update(self, request, *args, **kwargs):
+        comment = self.get_object()
+        serializer = CommentSerializer(comment, data=request.data)
+
+        if serializer.is_valid():
+            if comment.user == request.user:
+                serializer.save()
+                return Response(serializer.data)
+
+            return Response({"detail": "Not found."}, status.HTTP_404_NOT_FOUND)
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, *args, **kwargs):
+        comment = self.get_object()
+
+        if comment.user == request.user:
+            self.perform_destroy(comment)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        return Response({"detail": "Not found."}, status.HTTP_404_NOT_FOUND)
